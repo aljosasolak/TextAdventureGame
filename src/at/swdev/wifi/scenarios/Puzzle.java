0731ff3dc0a41dbbd367eac3643ac64a2b20b5f5
aljosasolak <aljosasolak@yahoo.com>
2024-02-21 00:01:04 +0100

    1.1 Changes in Battle class, elaboration of execute method

--- src/at/swdev/wifi/scenarios/Puzzle.java
+++ src/at/swdev/wifi/scenarios/Puzzle.java
@@ -14,7 +14,8 @@ public class Puzzle implements Scenario {
             Welcome to the world of unseen,
             Where wonders and magic convene.
             Before my gates, you must be keen,
-            Answer the question, from our Sphinx serene.\n""";
+            Answer the question, from our Sphinx serene.
+            """;
 
 
     //Riddles and answers
@@ -41,16 +42,19 @@ public class Puzzle implements Scenario {
     List<String> answers = List.of(answer1, answer2, answer3);
 
 
-    Scanner input = new Scanner(System.in);
-    Random random = new Random();
-    int index = random.nextInt(3);
+
 
     @Override
     public void execute(Character character) {
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+        int index = random.nextInt(3);
+
         System.out.println(taskMessage);
         System.out.println(riddles.get(index));
         System.out.println(hint);
         boolean isAnswered = false;
+
 
         while (!(isAnswered)) {
             String userAnswer = input.nextLine();
@@ -58,22 +62,23 @@ public class Puzzle implements Scenario {
                 System.out.printf("""
                         Congratulations, %s, I've seen,
                         You merit to enter the world of unseen!""", character.getName());
-                input.close();
                 isAnswered = true;
+                input.close();
             } else if (userAnswer.equalsIgnoreCase("h")) {
                 System.out.printf("""
                         In the riddle of the sphinx, you must not feign.
                         The answer is %s, type your answer again.\n""", answers.get(index));
-                input.nextLine();
             } else if (!(userAnswer.equalsIgnoreCase(answers.get(index)))) {
                 System.out.printf("""
                         In the sphinx's lair, where wisdom soars,
                         %s was the answer, alas, not yours,
                         Denied is entry through these ancient doors.
                         Type your answer again, with thought that explores.\n""", answers.get(index));
-                input.nextLine();
             }
+
         }
+
+
     }
 
 
--- src/at/swdev/wifi/scenarios/Scenario.java
+++ src/at/swdev/wifi/scenarios/Scenario.java
@@ -3,8 +3,7 @@ package at.swdev.wifi.scenarios;
 import at.swdev.wifi.characters.Character;
 
 public interface Scenario {
-    String hint = "<for help press: h>\n";
+    String hint = "<for help press: h>";
     void execute (Character character);
 
-    //void hint();
 }
