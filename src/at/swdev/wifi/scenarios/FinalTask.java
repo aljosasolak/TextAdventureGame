2eb5086e0a58f618050540b55b7a1faf1da0b6bf
aljosasolak <aljosasolak@yahoo.com>
2024-02-21 17:35:34 +0100

    Fixed Bug with the execute method in the FinalTask

--- src/at/swdev/wifi/scenarios/FinalTask.java
+++ src/at/swdev/wifi/scenarios/FinalTask.java
@@ -6,20 +6,18 @@ import java.util.Scanner;
 
 public class FinalTask implements Scenario {
 
-    //private Character player;
-    private final String finalTaskMessage = """       
+    private String finalTaskMessage = """       
             In battles fierce, you conquered, set yourself free,
             Towards the source of magic, where wisdom be.
             To end the quest, your first answer must the last one be,
             Choose wisely, wrong answers, they weaken thee.
             <type your first answer>""";
 
-    private final String taskNotPassedMessage = """
+    private String taskNotPassedMessage = """
             Your first answer was '%s', brave traveler.
             This time you didn't reach the source of magic and wisdom,
             but fear not and try again! 😊
             """;
-
 
     @Override
     public void execute(Character player) {
@@ -28,38 +26,29 @@ public class FinalTask implements Scenario {
         Scanner input = new Scanner(System.in);
 
         while (player.getHealth() > 0 && player.getStrength() > 0) {
+
             if (player.getName().equalsIgnoreCase(input.nextLine())) {
+                System.out.println("That is correct!");
                 System.out.println("You proved to be the true master of wisdom and magic!");
                 break;
-                //The conditions should be defined better
-                /*Messages like this appear
-                The wrong answer has weakened you!
-                Your health: 10
-                Your strength: 0
-                Your name is your clue,
-                for it holds the key to the magic pure and true
-                Try your luck again:
-                Game over
-                 */
-            } else if (player.getStrength() > 34 && player.getHealth() > 30) {
-                player.setStrength(player.getStrength() - 17);
-                player.setHealth(player.getHealth() - 15);
-                System.out.println("The wrong answer has weakened you!");
-                System.out.printf("Your strength: %d\n", player.getStrength());
-                System.out.printf("Your health: %d\n", player.getHealth());
-                System.out.println("Try your luck again: ");
-            } else if (player.getStrength() <= 34 || player.getHealth() <= 30) {
-                player.setStrength(player.getStrength() - 17);
-                player.setHealth(player.getHealth() - 15);
-                System.out.println("The wrong answer has weakened you!");
-                System.out.printf("Your strength: %d\n", player.getStrength());
-                System.out.printf("Your health: %d\n", player.getHealth());
-                System.out.println("Your name is your clue,\nfor it holds the key to the magic pure and true");
-                System.out.println("Try your luck one last time: ");
             } else {
-                System.out.printf(taskNotPassedMessage, player.getName());
-                player.setHealth(0);
-                player.setStrength(0);
+                player.setStrength(player.getStrength() - 17);
+                player.setHealth(player.getHealth() - 15);
+                System.out.println("The wrong answer has weakened you!");
+                System.out.printf("Your strength: %d\n", player.getStrength());
+                System.out.printf("Your health: %d\n", player.getHealth());
+                if (player.getStrength() > 17 && player.getHealth() > 15) {
+                    System.out.println("Try your luck again: ");
+                } else if (0 < player.getStrength() & player.getStrength() <= 17) {
+                    System.out.println("Your name is your clue,\nfor it holds the key to the magic pure and true");
+                    System.out.println("Try your luck one last time: ");
+                } else if (0 < player.getHealth() & player.getHealth() <= 15) {
+                    System.out.printf("Your health: %d\n", player.getHealth());
+                    System.out.println("Your name is your clue,\nfor it holds the key to the magic pure and true");
+                    System.out.println("Try your luck one last time: ");
+                } else {
+                    System.out.printf(taskNotPassedMessage, player.getName());
+                }
             }
         }
     }
